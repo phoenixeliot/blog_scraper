ec20e70d7edf8c0542b5556561e85e535a092134
Phoenix Eliot <phoenixjeliot@gmail.com>
2018-10-30 13:11:03 -0700

    Replacing Guilt

--- worm_scraper.rb
+++ worm_scraper.rb
@@ -3,8 +3,27 @@ require 'open-uri'
 require 'uri'
 require 'base64'
 
+@domain = 'http://mindingourway.com'
+def ensure_domain(url)
+  if url.include? 'http'
+    return url
+  else
+    return @domain + url
+  end
+end
+
+@index_page = 'http://mindingourway.com/guilt/'
+doc = Nokogiri::HTML(open(@index_page))
+@chapters = doc.css('.post-content a').map { |a| a['href'] }
+$stderr.puts @chapters
+
+@mode = :indexed # :indexed or :sequential
 #set to first chapter
-@next_chapter = 'https://sinceriously.fyi/inconceivable/'
+if @mode == :indexed
+  @next_chapter = ensure_domain(@chapters.shift())
+else
+  @next_chapter = 'http://mindingourway.com/half-assing-it-with-everything-youve-got/'
+end
 # @next_chapter = 'https://sinceriously.fyi/false-faces/'
 @toc = "<h1>Table of Contents</h1>"
 @book_body = ""
@@ -12,21 +31,22 @@ require 'base64'
 
 while @next_chapter
   #check if url is weird
-  @next_chapter.sub!('http://', 'https://')
+  @next_chapter.sub!('https://', 'http://')
   if @next_chapter.to_s.include?("½")
     @next_chapter = URI.escape(@next_chapter)
   end
   doc = Nokogiri::HTML(open(@next_chapter))
   #get
-  @chapter_title = doc.css('h1.entry-title').first #html formatted
+  @chapter_title = doc.css('.post-title').first #html formatted
 
   #modify chapter to have link
   @chapter_title_plain = @chapter_title.content
   $stderr.puts @chapter_title_plain
-  @chapter_content = doc.css('div.entry-content').first #gsub first p
+  # puts doc
+  @chapter_content = doc.css('.post-content').first #gsub first p
   #clean
   @chapter_content.search('.//div').remove
-  @to_remove = doc.css('div.entry-content p').first #gsub first p
+  # @to_remove = doc.css('.post-content p').first #gsub first p
   @chapter_content = @chapter_content.to_s#.gsub(@to_remove.to_s,"")
   #write
   @book_body << "<h1 id=\"chap#{@index.to_s}\">#{@chapter_title_plain}</h1>"
@@ -34,23 +54,31 @@ while @next_chapter
   @toc << "<a href=\"#chap#{@index.to_s}\">#{@chapter_title_plain}</a><br>"
   @index += 1
   #next
-  @next_chapter = if doc.css('div.nav-next a').last&.content.to_s.include?("Next")
-                    doc.css('div.nav-next a').last['href']
-                  else
-                    false
-                  end
+  if @mode == :indexed
+    @next_chapter = @chapters.shift()
+  else
+    @next_chapter = if doc.css('.right.navlink').last
+                      doc.css('.right.navlink').last['href']
+                    else
+                      false
+                    end
+  end
+  if @next_chapter
+    @next_chapter = ensure_domain(@next_chapter)
+  end
   # @next_chapter = false #nocommit
 end
 
 @book_body.scan(/<img.*?src="(.*?)".*?>/).each_with_index { |match, i|
-  imgUrl = match[0]
+  img_url = match[0]
   # File.open("./sinceriously/#{i}.png", 'wb') { |fo|
-  #   fo.write(open(imgUrl).read)
+  #   fo.write(open(img_url).read)
   # }
-  # @book_body.sub!(imgUrl, "#{i.to_s}.png")
-  
-  encoded = Base64.encode64(open(imgUrl).read)
-  @book_body.sub!(imgUrl, "data:image/png;base64,#{encoded}")
+  # @book_body.sub!(img_url, "#{i.to_s}.png")
+  $stderr.puts "Image URL:", img_url, ensure_domain(img_url)
+  full_img_url = ensure_domain(img_url)
+  encoded = Base64.encode64(open(full_img_url).read)
+  @book_body.gsub!(img_url, "data:image/png;base64,#{encoded}")
 }
 
 $stderr.puts "Writing Book..."
