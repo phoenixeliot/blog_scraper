e0c70752ade6ef3d1296c8b276c12dee2926619f
Phoenix Eliot <phoenixjeliot@gmail.com>
2018-11-10 10:57:06 -0800

    Fix agenty duck image replacer

--- blog_configs/agenty_duck.py
+++ blog_configs/agenty_duck.py
@@ -1,5 +1,5 @@
 def rewrite_post(post):
     for soup in post['body_soups']:
         for element in soup.select('img[src]'):
-            if element['src'] == 'http://www.wallpaper77.com/upload/DesktopWallpapers/cache/Kodama-ipad-3-wallpaper-ipad-wallpaper-retina-display-wallpaper-the-new-ipad-wallpaper--1600x1200.jpg'
+            if element['src'] == 'http://www.wallpaper77.com/upload/DesktopWallpapers/cache/Kodama-ipad-3-wallpaper-ipad-wallpaper-retina-display-wallpaper-the-new-ipad-wallpaper--1600x1200.jpg':
                 element['src'] = 'https://web.archive.org/web/20170818074822if_/'
